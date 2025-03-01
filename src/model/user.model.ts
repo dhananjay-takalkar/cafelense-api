8d9ba21513e053fd7a6a24c09604b7115b198f63
Dhananjay Takalkar <dhananjay.infimax@gmail.com>
2025-03-01 20:38:42 +0400

    added change for new data creation

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -43,7 +43,7 @@ const userSchema = new Schema(
 const User = mongoose.model<IUser & Document>("User", userSchema);
 
 userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
+  if (this.isModified("password") || this.isNew) {
     this.password = await bcrypt.hash(this.password, 10);
   }
   next();
