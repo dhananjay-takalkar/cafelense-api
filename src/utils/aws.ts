c77a1bd474b094b59e6ac449c987981a3a5c44d1
Dhananjay Takalkar <dhananjay.infimax@gmail.com>
2025-03-02 15:03:06 +0400

    added change for image name

--- src/utils/aws.ts
+++ src/utils/aws.ts
@@ -33,7 +33,9 @@ export const uploadImageToS3 = async (
     process.env.AWS_BUCKET_NAME = "playzoneapp";
     const params = {
       Bucket: process.env.AWS_BUCKET_NAME || "playzoneapp",
-      Key: `dishes/${cafe_id}_${name.trim().replace(/\s+/g, "_")}`,
+      Key: `dishes/${cafe_id}_${name
+        .trim()
+        .replace(/\s+/g, "_")}_${Date.now()}`,
       Body: file.buffer,
       ContentType: file.mimetype,
       ACL: "public-read" as ObjectCannedACL,
